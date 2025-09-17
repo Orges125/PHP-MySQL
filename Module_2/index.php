8bec8705c91c11160d7d6ec6d94ec9f1c8faacdc
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-09-17 20:46:26 +0200

    PHP Variables

---
+++ Module_2/index.php
@@ -0,0 +1,34 @@
+<?php
+     $school = 'Digital School';
+
+     echo"I love $school<br>";
+
+     $x= 120;
+     $y=50;
+     echo $x + $y; echo "<br>";
+     echo $x * $y; echo "<br>";
+     echo $x - $y; echo "<br>";
+     echo $x / $y; echo "<br>";
+     echo $x % $y; echo "<br>";
+
+     $a = 'Digtal';
+     $b = 'School';
+     $c = $a.$b;
+
+     echo "$c \n";
+
+     $the_string='Digital School'; echo "<br>";
+     echo strlen($the_string); echo "<br>";
+
+     echo str_word_count($the_string); echo "<br>";
+
+     $Programming = "Programming is very cool";
+     echo str_replace ("not" , "very" , $Programming); echo "<br>";
+
+     $the_string='Programming';
+    echo strrev ($the_string);
+
+     
+
+
+?>
