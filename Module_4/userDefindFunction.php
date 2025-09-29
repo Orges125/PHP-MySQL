72e380a582817e3443a3afd65dfd76f90f720e14
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-09-29 20:40:10 +0200

    function

---
+++ Module_4/userDefindFunction.php
@@ -0,0 +1,43 @@
+<?php
+    
+    function helloWorld(){
+        echo "Hello World";
+    }
+    helloWorld();
+
+    echo "<br>";
+
+    # example 2
+
+    function sum(){
+        $value = 120 + 20;
+        echo $value;
+    }
+    sum();
+    echo "<br>";
+
+    
+
+    function maximum ($x , $y){
+
+        if ($x > $y){
+           return $x;
+
+       }else{
+        return $y;
+       }
+
+    }
+       
+
+       $a = 20;
+       $b = 30;
+        $test = maximum ($a , $b);
+        echo "The max of $a and $b is $test \n"
+         
+
+         
+
+       
+
+?>
