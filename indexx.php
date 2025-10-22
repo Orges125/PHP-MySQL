fb1633bfc44f1352b41a00348ba7b6b93b712725
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-10-22 20:45:41 +0200

    config

---
+++ indexx.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="add.php" method="POST">
+        <label for="Username">Username</label>
+        <input type="text" name="username" placeholder="Username"><br><br>
+        <input type="text" name="email" placeholder="email"><br><br>
+        <input type="submit" value="Submit" name="submit"><br>
+     </form>
+    
+</body>
+</html>
