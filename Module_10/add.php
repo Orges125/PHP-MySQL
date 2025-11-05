bf795740ab85fa44482cd95e256614066a0be5a1
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-11-05 20:47:12 +0100

    dashboard

--- Module_10/add.php
+++ Module_10/add.php
@@ -1,23 +1,47 @@
 <?php
-     include_once("config.php");
 
-     if(isset($_POST['submit'])){
 
-        $name = $_POST ['name'];
-        $surname = $_POST ['surname'];
-        $email = $_POST ['email'];
+    include_once('config.php'); 
 
-        $sql = "INSERT INTO users (name,surname,email) VALUES (:name, :surname, :email)";
 
+
+    if(isset($_POST['submit']))
+    {
+        $name = $_POST['name'];
+        $surname = $_POST['surname'];
+        $email = $_POST['email'];
+
+
+        
+        $sql = "insert into users (name, surname, email) values (:name, :surname, :email)";
         $sqlQuery = $conn->prepare($sql);
+    
+        $sqlQuery->bindParam(':name', $name); 
+        $sqlQuery->bindParam(':surname', $surname); 
+        $sqlQuery->bindParam(':email', $email);
 
-        $sqlQuery->bindParam(':name',$name);
-        $sqlQuery->bindParam(':surname',$surname);
-        $sqlQuery->bindParam(':email',$email);
 
         $sqlQuery->execute();
 
-        echo "The user was added successfully!";
 
-     }
+        echo "Data saved successfully ...";
+    }
 ?>
+
+
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <a href="dashboard.php">Dashboard</a>
+        <form action="add.php" method="POST">
+            <input type="text" name="name" placeholder="Name"></br>
+            <input type="text" name="surname" placeholder="Surname"></br>
+            <input type="email" name="email" placeholder="Email"></br>
+            <button type="submit" name="submit">Add</button>
+        </form>
+
+
+    </body>
+</html>
+
--- Module_10/dashboard.php
+++ Module_10/dashboard.php
@@ -13,14 +13,41 @@
             padding: 10px 20px;
         }
         </style>
-        <?php
-        include 'db.php';
-        $sql ='SELECT * FROM users';
+        
+        
+    <?php
+        include_once('config.php');
+        $sql = "SELECT * FROM users";
         $getUsers = $conn->prepare($sql);
-        $getUsers->excute();
-        ?>
-</head>
-<body>
-    
+        $getUsers->execute();
+        $users=$getUsers->fetchAll();
+    ?>
+    <br> <br>
+
+
+    <table>
+        <thead>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+        </thead>
+    <tbody>
+        <?php
+            foreach($users as $user){
+?> 
+    <tr>
+        <td> <?= $user['id'] ?> </td>
+        <td> <?= $user['name'] ?> </td>
+        <td> <?= $user['surname'] ?> </td>
+        <td> <?= $user['email'] ?></td>
+    </tr>
+    <?php
+            }
+    ?>        
+        
+    </tbody>
+    </table>
+    <a href="add.php">Add User</a>
 </body>
 </html>
